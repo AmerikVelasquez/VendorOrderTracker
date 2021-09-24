a7d324e447de0ab70a14f6dd47aff4d84749d181
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-09-24 14:08:17 -0700

    add id for vendor

--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -7,12 +7,14 @@ namespace VendorOrderTracker.Models
     public string Name {get; set;}
     public string Description {get; set;}
     private static List<Vendor> _instances = new List<Vendor> {};
+    public int Id {get; }
 
     public Vendor(string name, string description)
     {
       Name = name;
       Description = description;
       _instances.Add(this); 
+      Id = _instances.Count;
     }
 
     public static List<Vendor> GetAll()
@@ -23,6 +25,11 @@ namespace VendorOrderTracker.Models
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId -1];
     }
   }
 }
